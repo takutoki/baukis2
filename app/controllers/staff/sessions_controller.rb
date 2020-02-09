2a1f45345e202238927dd7d6800518ba2d848c5f
takutoki <34600200+takutoki@users.noreply.github.com>
2020-02-09 15:35:39 +0900

    stylesheet適用

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -12,10 +12,20 @@ class Staff::SessionsController < Staff::Base
   end
 
   def create
-
+    @form = Staff::LoginForm.new(params[:staff_login_form])
+    if @form.email.present?
+      staff_member = StaffMember.find_by("LOWER(:email) = ?", @form.email.downcase)
+    end
+    if staff_member
+      session[:staff_member_id] = staff_member.id
+      redirect_to :staff_root
+    else
+      render action: :new
+    end
   end
 
   def destroy
-
+    session.delete(:staff_memner_id)
+    redirect_to :staff_root
   end
 end
